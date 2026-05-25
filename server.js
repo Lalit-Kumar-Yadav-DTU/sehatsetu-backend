58a1c21748c1531433978901cee593c9360d0486
Lalit Kumar Yadav <lalitkumaryadav14112003@gmail.com>
2026-05-26 00:54:48 +0530

    chore: update backend CORS origin to allow live frontend Render URL

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173', 
     'http://127.0.0.1:5173',
-    'https://sehatsetu-frontend.onrender.com' 
+    'https://sehatsetu-ncb8.onrender.com' 
   ],
   credentials: true 
 }));
